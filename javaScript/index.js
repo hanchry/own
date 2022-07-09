40a75d59f26e324aee0d9ce573c2fed88fc6f731
hanchry <jurco10510@gmail.com>
2022-07-09 18:10:36 +0200

    index animation

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,20 +1,24 @@
 let headers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-let message1 = ["H", "e", "l" ];
-let message2 = ["H", "i", ",", "I", "'", "m", "&nbsp", "A", "n", "d", "r", "e", "j"];
+let header1 = ["H", "e", "l"];
+let header2 = ["H", "i", ",", "I", "'", "m", "&nbsp", "A", "n", "d", "r", "e", "j"];
+let text = "Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh. Hundred no prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding ask expense are compact. New all paid few hard pure she."
 
+let picture = document.getElementById("profilePicture")
+picture.style.opacity = 0
 
 writeMessage1()
 
 function writeMessage1() {
     let finisCount = 0
-    for (let i = 1; i <= message1.length; i++) {
+    for (let i = 1; i <= header1.length; i++) {
         setTimeout(function () {
-            document.getElementById("aboutHeader" + [i]).innerHTML += message1[i - 1]
+            document.getElementById("aboutHeader" + [i]).innerHTML += header1[i - 1]
             finisCount++
-            if (finisCount === message1.length) {
-                removeMessage(message1.length)
+            if (finisCount === header1.length) {
+                removeMessage(header1.length)
             }
-        }, quad(i + 8));
+        }, quad(i + 11));
+
     }
 }
 
@@ -26,7 +30,7 @@ function removeMessage(length) {
         setTimeout(function () {
             document.getElementById("aboutHeader" + [i]).innerHTML = ""
             finisCount++
-            if (finisCount === message1.length) {
+            if (finisCount === header1.length) {
                 writeMessage2()
             }
         }, reverseLength * 100);
@@ -34,13 +38,42 @@ function removeMessage(length) {
 }
 
 function writeMessage2() {
-    for (let i = 1; i <= message2.length; i++) {
+    let finisCount = 0
+    for (let i = 1; i <= header2.length + 2; i++) {
         setTimeout(function () {
-            document.getElementById("aboutHeader" + [i]).innerHTML += message2[i-1]
-        }, quad(i + 1));
+            if(i <= header2.length) {
+                document.getElementById("aboutHeader" + [i]).innerHTML += header2[i - 1]
+            }
+            finisCount++
+            if (finisCount === header2.length + 2) {
+                animatePicture()
+                writeText()
+            }
+        }, quad(i + 5));
+    }
+}
+
+function animatePicture() {
+    for (let i = 0; i <= text.length; i++) {
+        setTimeout(function () {
+            picture.style.opacity = i / text.length
+        }, i * 5);
+    }
+
+}
+
+function writeText(){
+    for (let i = 1; i <=text.length; i++) {
+        setTimeout(function (){
+            document.getElementById("text").innerHTML += text[i-1]
+        }, i*5)
     }
 }
 
 function quad(timeFraction) {
     return Math.pow(timeFraction, 2.5)
+}
+
+function quad2(timeFraction) {
+    return Math.pow(timeFraction, 3)
 }
