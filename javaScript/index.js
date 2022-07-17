5ba7fd5cd196eef356c5141ec0619877433bff94
hanchry <jurco10510@gmail.com>
2022-07-17 19:27:05 +0200

    use of promises for animations different justifying for phone and desktop

--- javaScript/index.js
+++ javaScript/index.js
@@ -3,77 +3,72 @@ let header1 = ["H", "e", "l"];
 let header2 = ["H", "i", ",", "I", "'", "m", "&nbsp", "A", "n", "d", "r", "e", "j"];
 let text = "Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh. Hundred no prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding ask expense are compact. New all paid few hard pure she."
 
-let picture = document.getElementById("profilePicture")
-picture.style.opacity = 0
+let headerDelay = 100
+let textDelay = 5
+let pictureDelay = 5
 
-writeMessage1()
 
-function writeMessage1() {
-    let finisCount = 0
-    for (let i = 1; i <= header1.length; i++) {
-        setTimeout(function () {
-            document.getElementById("aboutHeader" + [i]).innerHTML += header1[i - 1]
-            finisCount++
-            if (finisCount === header1.length) {
-                removeMessage(header1.length)
-            }
-        }, quad(i + 11));
 
-    }
+function getLetter(message, i, delay) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(message[i]);
+        }, delay);
+    });
 }
 
-function removeMessage(length) {
-    let reverseLength = 0
-    let finisCount = 0
-    for (let i = length; i > 0; i--) {
-        reverseLength++
-        setTimeout(function () {
-            document.getElementById("aboutHeader" + [i]).innerHTML = ""
-            finisCount++
-            if (finisCount === header1.length) {
-                writeMessage2()
-            }
-        }, reverseLength * 100);
-    }
+function getOpacity(i, delay) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(i);
+        }, delay);
+    });
 }
 
-function writeMessage2() {
-    let finisCount = 0
-    for (let i = 1; i <= header2.length + 2; i++) {
-        setTimeout(function () {
-            if(i <= header2.length) {
-                document.getElementById("aboutHeader" + [i]).innerHTML += header2[i - 1]
-            }
-            finisCount++
-            if (finisCount === header2.length + 2) {
-                animatePicture()
-                writeText()
-            }
-        }, quad(i + 5));
-    }
-}
-
-function animatePicture() {
+async function animatePicture() {
     for (let i = 0; i <= text.length; i++) {
-        setTimeout(function () {
-            picture.style.opacity = i / text.length
-        }, i * 5);
-    }
-
-}
-
-function writeText(){
-    for (let i = 1; i <=text.length; i++) {
-        setTimeout(function (){
-            document.getElementById("text").innerHTML += text[i-1]
-        }, i*5)
+        const x = await getOpacity(i, pictureDelay );
+        document.getElementById("profilePicture").style.opacity = x / text.length
     }
 }
 
-function quad(timeFraction) {
-    return Math.pow(timeFraction, 2.5)
+async function writeHeader1() {
+    for (let i = 1; i < header1.length + 1; i++) {
+        const x = await getLetter(header1, i - 1, Math.pow(i + 5, 2));
+        document.getElementById("aboutHeader" + [i]).innerHTML += x
+    }
 }
 
-function quad2(timeFraction) {
-    return Math.pow(timeFraction, 3)
+async function writeHeader2() {
+    for (let i = 1; i < header2.length + 1; i++) {
+        const x = await getLetter(header2, i - 1, Math.pow(i + 4, 2));
+        document.getElementById("aboutHeader" + [i]).innerHTML += x
+    }
 }
+
+async function deleteHeader1() {
+    for (let i = header1.length; i > 0; i--) {
+        const x = await getLetter(header1, i - 1, headerDelay);
+        document.getElementById("aboutHeader" + [i]).innerHTML = ""
+    }
+}
+
+async function writeText() {
+    for (let i = 1; i < text.length + 1; i++) {
+        const x = await getLetter(text, i - 1, textDelay);
+        document.getElementById("text").innerHTML += x
+    }
+}
+
+
+async function animation() {
+    await writeHeader1()
+    await deleteHeader1()
+    await writeHeader2()
+    setTimeout(() => {
+        writeText()
+        animatePicture()
+    }, 500)
+}
+
+animation()
