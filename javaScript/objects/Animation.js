48d4a4326e6af22e62522ab8167b545434767862
hanchry <jurco10510@gmail.com>
2022-07-20 13:52:19 +0200

    js separation

---
+++ javaScript/objects/Animation.js
@@ -0,0 +1,26 @@
+
+export default class Animation {
+
+    animateDropIn(elementId, isImage) {
+        if (!isImage) {
+            if(window.screen.width > 1024) {
+                document.getElementById(elementId).style.animation = "dropInMedium 1s ease forwards"
+                document.getElementById(elementId).style.visibility = "visible"
+            }
+            else {
+                document.getElementById(elementId).style.animation = "dropInSmall 1s ease forwards"
+                document.getElementById(elementId).style.visibility = "visible"
+            }
+        }
+        else {
+            if(window.screen.width > 1024) {
+                document.getElementById(elementId).style.animation = "dropInMedium 1s ease forwards"
+                document.getElementById(elementId).style.opacity = "1"
+            }
+            else {
+                document.getElementById(elementId).style.animation = "dropInSmall 1s ease forwards"
+                document.getElementById(elementId).style.opacity = "1"
+            }
+        }
+    }
+}
---
+++ javaScript/objects/Tiping.js
@@ -0,0 +1,26 @@
+
+export default class Tiping {
+
+    async writeHeader(elementId, header, delay) {
+        for (let i = 1; i < header.length + 1; i++) {
+            const x = await this.getLetter(header, i - 1, Math.pow(i + delay, 2));
+            console.log(elementId)
+            document.getElementById(elementId + [i]).innerHTML += x
+        }
+    }
+
+    async deleteHeader(elementId, header, delay) {
+        for (let i = header.length; i > 0; i--) {
+            const x = await this.getLetter(header, i - 1, delay);
+            document.getElementById(elementId + [i]).innerHTML = ""
+        }
+    }
+
+    getLetter(message, i, delay) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(message[i]);
+            }, delay);
+        });
+    }
+}
